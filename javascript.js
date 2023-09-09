eb6133d004efe9f1ae43424442756be815c724da
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-09-08 21:44:47 -0700

    Add matching password validation

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 const requiredInputs = document.querySelectorAll('input[required]');
 const submitButton = document.querySelector('button[type="submit"]');
+const passwordFields = document.querySelectorAll('[type="password"]');
+const passwordSpan = document.querySelector('span.password');
 
 submitButton.addEventListener('click', () => {
     requiredInputs.forEach(input => input.className = 'submitted');
+    if (passwordFields[0].value !== passwordFields[1].value) {
+        passwordSpan.classList.add('validation');
+        passwordFields.forEach(field => {
+            field.className = 'validation';
+        });
+    } else {
+        passwordSpan.classList.remove('validation');
+    }
 });
